bc4acca1c63ea15f179eb63f6c37e23308e34dfd
3205244328 <3205244328@qq.om>
2020-05-17 11:02:14 +0800

    07

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { TestAService } from './testa.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ch07';
+
+  constructor(private aService: TestAService) {
+    this.aService.setTitle('父组件');
+    this.title = this.aService.getTitle();
+  }
+
+  refresh() {
+    this.title = this.aService.getTitle();
+  }
+}
+
+
